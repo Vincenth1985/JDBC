9fe9f9234b055a3ccf846381a804223f2a08c514
Vincent Honca <vincenth1985@gmail.com>
2019-08-29 10:49:51 +0200

    testing connection with mysql

--- src/main/java/be/intecbrussel/Main.java
+++ src/main/java/be/intecbrussel/Main.java
@@ -1,15 +1,27 @@
 package be.intecbrussel;
 
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
 public class Main {
 
 
     public static void main(String[] args) {
 
-        Connection connection = DriverManager.getConnection("jbdc:mysql://localhost:3306/beers.db","root","");
+        try {
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/beersdb?serverTimezone=UTC","root","");
+            try{
+                PreparedStatement query = connection.prepareStatement("SELECT * FROM Categories");
+                ResultSet rs = query.executeQuery();
+                while(rs.next()){
+                    System.out.print(rs.getInt("id") + " - ");
+                    System.out.println(rs.getString("category"));
+                }
+            } catch (Exception e){}
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
 
 
     }
