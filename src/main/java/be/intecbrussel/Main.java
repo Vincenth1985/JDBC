fc5d2fe26ef2e91b67ca825e61d68328154d0c89
Vincent Honca <vincenth1985@gmail.com>
2019-08-29 10:29:43 +0200

    making connection with my sql

--- src/main/java/be/intecbrussel/Main.java
+++ src/main/java/be/intecbrussel/Main.java
@@ -1,6 +1,17 @@
 package be.intecbrussel;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Main {
 
-    
+
+    public static void main(String[] args) {
+
+        Connection connection = DriverManager.getConnection("jbdc:mysql://localhost:3306/beers.db","root","");
+
+
+    }
+
 }
