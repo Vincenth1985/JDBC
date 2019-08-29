d9e83f1d633fb48a2e5b424cf35a04041e89f2e5
Vincent Honca <vincenth1985@gmail.com>
2019-08-29 11:12:48 +0200

    query.executeQuery test

--- src/main/java/be/intecbrussel/Main.java
+++ src/main/java/be/intecbrussel/Main.java
@@ -6,22 +6,47 @@ import java.sql.*;
 public class Main {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
-        try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/beersdb?serverTimezone=UTC","root","");
-            try{
-                PreparedStatement query = connection.prepareStatement("SELECT * FROM Categories");
+
+        PreparedStatement query;
+
+//        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/beersdb?serverTimezone=UTC", "root", "")) {
+//
+//            try {
+//                query = connection.prepareStatement("SELECT * FROM Categories");
+//                ResultSet rs = query.executeQuery();
+//                while (rs.next()) {
+//                    System.out.print(rs.getString("id") + " - ");
+//                    System.out.println(rs.getString("category"));
+//                }
+//            } catch (Exception e) {
+//            }
+//
+//        } catch (SQLException e) {
+//            e.printStackTrace();
+//        }
+
+
+        try (Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/beersdb?serverTimezone=UTC", "root", "")) {
+
+            try {
+                query = connection.prepareStatement("SELECT name,alcohol FROM Beers");
                 ResultSet rs = query.executeQuery();
-                while(rs.next()){
-                    System.out.print(rs.getInt("id") + " - ");
-                    System.out.println(rs.getString("category"));
+                while (rs.next()) {
+                    System.out.print(rs.getString("name") + " - ");
+                    System.out.println(rs.getString("alcohol"));
+//
+
                 }
-            } catch (Exception e){}
+            } catch (Exception e) {
+            }
 
         } catch (SQLException e) {
             e.printStackTrace();
         }
+
+
 
 
     }
