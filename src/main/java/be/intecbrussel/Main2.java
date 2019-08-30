be905bfe6540b79192b897084ea4a11f273c3f35
Vincent Honca <vincenth1985@gmail.com>
2019-08-30 09:17:42 +0200

    Testing Prepared Statement

--- src/main/java/be/intecbrussel/Main2.java
+++ src/main/java/be/intecbrussel/Main2.java
@@ -7,20 +7,36 @@ public class Main2 {
     public static void main(String[] args) {
 
 
-        String delete = "Delete from Beers where name like 'test'";
-        String create = "Insert into Beers (name) Values('TEST')";
+        String delete = "Delete from Beers where name like 'DUVEL'";
+        String create = "Insert into Beers (name) Values('DUVEL')";
+        String update = "Update Beers SET price = ? where name like ?";
 
+        int id;
 
         try (Connection connection = new Dbconnector().getConnection();
-             Statement statement = connection.createStatement()) {
+        ) {
 
+            PreparedStatement query = connection.prepareStatement(update);
+            query.setFloat(1, 2);
+           // query.setString(2, "Duvel groen");
+            query.executeUpdate();
 
-            ResultSet rs = statement.executeQuery("Select * from Beers");
+            query = connection.prepareStatement("Select * from Beers where name like 'duvel groen'");
+            ResultSet rs = query.executeQuery();
+
+//            query.executeUpdate(create, Statement.RETURN_GENERATED_KEYS);
+//            ResultSet rs1 = query.getGeneratedKeys();
+//
+//            if (rs1.next()) {
+//                id = rs1.getInt(1);
+//                System.out.println(id);
+//            }
 
 
             while (rs.next()) {
                 System.out.print(rs.getString("id") + " - ");
-                System.out.println(rs.getString("name"));
+                System.out.print(rs.getString("name") + " - ");
+                System.out.println(rs.getString("price"));
             }
 
 
